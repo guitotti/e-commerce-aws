63b282a259cf5121bd2bd4c9fb4fb61bac8f7b8e
Guilherme Totti <dev.guilhermetotti@gmail.com>
2026-07-29 20:52:44 -0300

    feat: create products ddb in ProductsAppStack class

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -1,25 +1,44 @@
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as lambdaNodeJs from "aws-cdk-lib/aws-lambda-nodejs";
 import * as cdk from "aws-cdk-lib";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
 import { Construct } from "constructs";
+import { PartitionKey } from "aws-cdk-lib/aws-appsync";
 
 export class ProductsAppStack extends cdk.Stack {
   readonly productsFetchHandler: lambdaNodeJs.NodejsFunction;
+  readonly productsDdb: dynamodb.Table;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    this.productsFetchHandler = new lambdaNodeJs.NodejsFunction(this, "ProductsFetchFunction", {
-      functionName: "ProductsFetchFunction",
-      entry: "lambda/products/productsFetchFunction.ts",
-      handler: "handler",
-      memorySize: 128,
-      timeout: cdk.Duration.seconds(5), // tempo de duração máxima da lambda
-      bundling: {
-        minify: true,
-        sourceMap: false
-      }
-    })
+    this.productsDdb = new dynamodb.Table(this, "ProductsDdb", {
+      tableName: "products",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      partitionKey: {
+        name: "id",
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PROVISIONED,
+      readCapacity: 1,
+      writeCapacity: 1,
+    });
+
+    this.productsFetchHandler = new lambdaNodeJs.NodejsFunction(
+      this,
+      "ProductsFetchFunction",
+      {
+        functionName: "ProductsFetchFunction",
+        entry: "lambda/products/productsFetchFunction.ts",
+        handler: "handler",
+        memorySize: 128,
+        timeout: cdk.Duration.seconds(5), // tempo de duração máxima da lambda
+        bundling: {
+          minify: true,
+          sourceMap: false,
+        },
+      },
+    );
   }
 }
