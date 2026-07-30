ef5201b22cec89896a64d6264bb8d21f4f605030
Guilherme Totti <dev.guilhermetotti@gmail.com>
2026-07-29 21:20:39 -0300

    feat: add other operations Api Gateway products service

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -7,6 +7,7 @@ import { Construct } from "constructs";
 
 interface ECommerceApiStackProps extends cdk.StackProps {
   productsFetchHandler: lambdaNodeJs.NodejsFunction;
+  productsAdminHandler: lambdaNodeJs.NodejsFunction;
 }
 
 export class ECommerceApiStack extends cdk.Stack {
@@ -36,8 +37,25 @@ export class ECommerceApiStack extends cdk.Stack {
       props.productsFetchHandler,
     );
 
-    // "/products"
+    // GET /products
     const productsResource = api.root.addResource("products");
     productsResource.addMethod("GET", productsFetchIntegration);
+
+    // GET /products/{id}
+    const productIdResource = productsResource.addResource("{id}");
+    productIdResource.addMethod("GET", productsFetchIntegration);
+
+    const productsAdminIntegration = new apigateway.LambdaIntegration(
+      props.productsAdminHandler,
+    );
+
+    // POST /products
+    productsResource.addMethod("POST", productsAdminIntegration);
+
+    // PUT /products/id
+    productIdResource.addMethod("PUT", productsAdminIntegration);
+
+    // DELETE /products/id
+    productIdResource.addMethod("DELETE", productsAdminIntegration);
   }
 }
--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -4,10 +4,10 @@ import * as cdk from "aws-cdk-lib";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
 import { Construct } from "constructs";
-import { PartitionKey } from "aws-cdk-lib/aws-appsync";
 
 export class ProductsAppStack extends cdk.Stack {
   readonly productsFetchHandler: lambdaNodeJs.NodejsFunction;
+  readonly productsAdminHandler: lambdaNodeJs.NodejsFunction;
   readonly productsDdb: dynamodb.Table;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -29,21 +29,42 @@ export class ProductsAppStack extends cdk.Stack {
       this,
       "ProductsFetchFunction",
       {
+        runtime: lambda.Runtime.NODEJS_24_X,
+        memorySize: 512,
         functionName: "ProductsFetchFunction",
         entry: "lambda/products/productsFetchFunction.ts",
         handler: "handler",
-        memorySize: 128,
         timeout: cdk.Duration.seconds(5), // tempo de duração máxima da lambda
         bundling: {
           minify: true,
           sourceMap: false,
         },
         environment: {
-          PRODUCTS_DDB: this.productsDdb.tableName
-        }
+          PRODUCTS_DDB: this.productsDdb.tableName,
+        },
       },
     );
-
     this.productsDdb.grantReadData(this.productsFetchHandler);
+
+    this.productsAdminHandler = new lambdaNodeJs.NodejsFunction(
+      this,
+      "ProductsAdminFunction",
+      {
+        runtime: lambda.Runtime.NODEJS_24_X,
+        memorySize: 512,
+        functionName: "ProductsAdminFunction",
+        entry: "lambda/products/productsAdminFunction.ts",
+        handler: "handler",
+        timeout: cdk.Duration.seconds(5), // tempo de duração máxima da lambda
+        bundling: {
+          minify: true,
+          sourceMap: false,
+        },
+        environment: {
+          PRODUCTS_DDB: this.productsDdb.tableName,
+        },
+      },
+    );
+    this.productsDdb.grantWriteData(this.productsAdminHandler);
   }
 }
